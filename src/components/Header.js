556299fe9bdb5de87f8b5d40e9d0cc30abd57a18
bearboy27 <appalahemanth413@gmail.com>
2023-04-08 09:17:42 +0530

    Body-Completed,Header-Inprogress

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab esse in
+        tenetur aspernatur asperiores quam culpa voluptate beatae saepe itaque.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
