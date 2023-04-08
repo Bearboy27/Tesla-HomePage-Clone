a93930d19792b04f7f02fb0f860bdd437cb56a38
bearboy27 <appalahemanth413@gmail.com>
2023-04-08 11:28:01 +0530

    completed-home page clone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
   return (
-    <div>
-      <Container>
-        <a href="">
-          <img src="/images/logo.svg" alt="Tesla Logo" />
-        </a>
-        <Menu>
-          <a href="#">Model S</a>
+    <Container>
+      <a href="">
+        <img src="/images/logo.svg" alt="Tesla Logo" />
+      </a>
+      <Menu>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="#">
+              {car}
+            </a>
+          ))}
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <CustomMenu onClick={() => setBurgerStatus(true)}>
+          <MenuIcon />
+        </CustomMenu>
+      </RightMenu>
 
-          <a href="#">Model 3</a>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
 
-          <a href="#">Model X</a>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="#">{car}</a>
+            </li>
+          ))}
 
-          <a href="#">Model Y</a>
-        </Menu>
-        <RightMenu>
-          <a href="#">Shop</a>
-          <a href="#">Tesla Account</a>
-          <CustomMenu>
-            <MenuIcon />
-          </CustomMenu>
-        </RightMenu>
-      </Container>
-    </div>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Find Us</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+      </BurgerNav>
+    </Container>
   );
 }
 
@@ -43,6 +94,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -73,4 +125,38 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  overflow-y: auto;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
